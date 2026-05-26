0ab23c9cff6c902b860c6d69bd40a29180e625e5
paakhi123 <kataria9@uwindsor.ca>
2026-05-25 21:57:11 -0400

    Switch to Neon PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -6,12 +6,9 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = "sqlite:///./travel.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
